e86df3dd2d76db5e58bf47aa7c683880c89143a5
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-05-01 21:16:33 +0700

    Latihan

---
+++ i.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int pilihan;
+    do{
+    cout << "======= MENU =======" << endl;
+    cout  << "1. Hitung Luas Segitiga" << endl;
+    cout << "2. Hitung Keliling Segitiga" << endl;
+    cout << "3. Keluar" << endl;
+    cout << "Masukan Pilihan" << endl;
+    cin >> pilihan;  }while(pilihan);
+}
